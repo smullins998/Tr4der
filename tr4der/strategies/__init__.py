ce521ce789403489b19505812f4d5c1631d3bf93
smullins998 <Smullins998@gmail.com>
2024-09-30 11:50:09 -0400

    Remove TA

--- tr4der/strategies/__init__.py
+++ tr4der/strategies/__init__.py
@@ -1,6 +1,5 @@
 from .simple_strategies import SimpleStrategies
 from .technical_strategies import TechnicalAnalysisStrategies
 from .machine_learning_strategies import MachineLearningStrategies
-import ta
 
 __all__ = ['SimpleStrategies', 'TechnicalAnalysisStrategies', 'MachineLearningStrategies']
